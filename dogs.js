ca4a5afb27492565a73644d5926fb151d447b51a
Cassi Vance <cassivance@gmail.com>
2017-03-29 21:10:43 -0600

    Flex

--- dogs.js
+++ dogs.js
@@ -1,9 +1,9 @@
 let showTopTen = () => {
     document.getElementById("vote").style.display = "none";
-    document.getElementById("topTen").style.display = "block";
+    document.getElementById("topTen").style.display = "flex";
 }
 
 let vote = () => {
-    document.getElementById("vote").style.display = "block";
+    document.getElementById("vote").style.display = "flex";
     document.getElementById("topTen").style.display = "none";    
 }
